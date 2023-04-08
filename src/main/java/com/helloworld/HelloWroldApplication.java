43fa39b3d478a0da530d597237b52cd3cf02c075
HeoJiye <heoze1031@gmail.com>
2023-04-09 00:25:08 +0900

    rename: example 프로젝트 흔적 지우기

--- src/main/java/com/helloworld/HelloWroldApplication.java
+++ src/main/java/com/helloworld/HelloWroldApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactAndSpringApplication {
+public class HelloWroldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReactAndSpringApplication.class, args);
+		SpringApplication.run(HelloWroldApplication.class, args);
 	}
 }
