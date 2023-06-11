d5155e8f1f759f71a5e30f067de851c265dedbf0
Knock <chein1010@naver.com>
2023-06-12 00:15:43 +0900

    docker: add service

--- src/main/java/com/helloworld/controller/TestController.java
+++ src/main/java/com/helloworld/controller/TestController.java
@@ -1,29 +1,136 @@
 package com.helloworld.controller;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.helloworld.service.DockerService;
 
 @RestController
 @RequestMapping("/tests")
 public class TestController {
+	
+	@Autowired DockerService dockerService;
+	
 	@GetMapping("/{testId}")
     public List<String> get(@PathVariable long testId) {
 		// test의 정보 가져오기
 		return null;
     }
-	
-	@PostMapping("/{testId}/submits")
-    public List<String> submit(@PathVariable long testId) {
-		// 문제 풀이 제출
-		// 세션에는 최근 N개를 저장해 둠. 별개로 DB와 비교하여 최고점이면 DB insert
-		return null;
-    }
 
+	// 문제 풀이 제출
+	// 세션에는 최근 N개를 저장해 둠. 별개로 DB와 비교하여 최고점이면 DB insert
+    @PostMapping("/{testId}/submits")
+    public ResponseEntity<Map<String, Object>> submit (	HttpServletRequest request,
+    														@RequestParam String type,
+    														@RequestParam String code,
+    														@PathVariable long testId, Map<String, Object> model ) {
+    	HttpSession session = request.getSession();
+    	String submitId = new SimpleDateFormat("yyyyMMdd-HHmmss").format(Calendar.getInstance().getTime());
+    	String userId = (String) session.getAttribute("user_id");
+    	String path = new ClassPathResource("docker").getPath() + "/";
+    	String end = "";
+		List<String> require = new ArrayList<>();
+    	switch(type) {
+    		case "python":
+    			end = ".py";
+    			for(String c: code.split("\n")) {
+    				if(c.startsWith("import") || (c.startsWith("from"))) {
+    					require.add(c.split(" ")[1]);
+    				}
+    			}
+    	    	String rpath = path + "/" + userId + "/requirements.txt";
+    	    	File rfile = new File(rpath);
+    	    	try {
+    	    		BufferedWriter writer = new BufferedWriter(new FileWriter(rfile));
+    	    		writer.write(code);
+    	    		writer.close();
+    	    	} catch (IOException e) {
+    	    		e.printStackTrace();
+    	    	}
+    			break;
+    		case "java":
+    			end = ".java";
+    			for(String c: code.split("\n")) {
+    				if(c.startsWith("import")) {
+    					require.add(c.split(" ")[1]);
+    				}
+    			}
+    			break;
+    		case "c":
+    			end = ".c";
+    			for(String c: code.split("\n")) {
+    				if(c.startsWith("#include")) {
+    					require.add(c.split(" ")[1]);
+    				}
+    			}
+    			break;
+    		default:
+    			break;
+    	}
+    	String dpath = path + "/" + userId + "/" + testId + "_" + submitId + end;
+    	File dfile = new File(dpath);
+    	try {
+    		BufferedWriter writer = new BufferedWriter(new FileWriter(dfile));
+    		writer.write(code);
+    		writer.close();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	
+		String tag = userId + "_" + type;
+		String containerId = dockerService.getId(tag);
+    	if(containerId.equals("none")) {
+        	String cpath = new ClassPathResource("docker").getPath() + "/" + userId + "/Dockerfile_" + type;
+        	File cfile = new File(cpath);
+        	try {
+        		BufferedWriter writer = new BufferedWriter(new FileWriter(cfile));
+        		switch(type) {
+	        		case "python":
+		        		writer.write("FROM python:3\n");
+		        		writer.write("WORKDIR /usr/src/app\n");
+		        		if(!require.isEmpty()) {
+		        			writer.write("COPY ./requirements.txt ./\n");
+		        			writer.write("RUN pip install --no-cache-dir -r requirements.txt\r\n");
+		        		}
+		        		break;
+	        		case "java":
+	        			writer.write("FROM openjdk:8\n");
+	        			writer.write("WORKDIR /usr/src/app\n");
+        		}
+        		writer.close();
+        	} catch (IOException e) {
+        		e.printStackTrace();
+        	}        	
+    		containerId = dockerService.build(cpath, tag);
+    	} else {
+    		Map<Integer, String> result = dockerService.terminal(containerId, "docker run -i --rm --name " + tag + " -w /usr/src/myapp python:3 python " + dfile);
+    		String output = result.get(0);
+        	model.put("output", output);
+    	}
+    	return ResponseEntity.ok(model);
+    }
+	
+	
 	@GetMapping("/{testId}/submits")
     public List<String> getSubjectList(@PathVariable long userId) {
 		// DB에 있는 최고점 제출 정보 + session에 있는 최근 N개의 제출 정보
---
+++ src/main/java/com/helloworld/service/DockerService.java
@@ -0,0 +1,171 @@
+package com.helloworld.service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DockerService {
+
+	public String getId(String tag) {
+		String output = "";
+		Map<Integer, String> result = new HashMap<>();
+		try {
+			String cmd = "docker ps -a";
+			result = terminal(cmd);
+			output = result.get(0);
+			String[] dockers = output.split("\n");
+			for(String docker: dockers) {
+				if(docker.contains(tag)) {
+					String[] d = docker.split(" ");
+					return d[0];
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return "none";
+	}
+	
+	public String build(String filename, String tag) {
+		Map<Integer, String> result = new HashMap<>();
+		try {
+			String cmd = "docker build ";
+			if(tag != null) {
+				cmd += "-t " + tag + " ";
+			}
+			if(filename != null) {
+				cmd += "-f " + filename;
+			}
+			result = terminal(cmd);
+			if(result.get(0) == null) {
+				String key = result.keySet().toString();
+				throw new Exception(key + ": " + result.get(key));
+			}
+			String id = this.getId(tag);
+			return id;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return "none";
+	}
+	
+	public String test(String id, String type, String input) {
+		String output = "";
+		Map<Integer, String> result = new HashMap<>();
+		try {
+			switch(type) {
+				case "c":
+					result = terminal("docker exec -i " + id + " /bin/bash -c ", "\"cd ~/test/c && echo -e '" + input + "' >> test.c\"");
+					result = terminal("docker exec -i " + id + " /bin/bash -c ", "\"cd ~/test/c && gcc test.c -o test.o\"");
+					result = terminal("docker exec -i " + id + " /bin/bash -c ", "\"cd ~/test/c && ./test.o\"");
+					output = result.get(0);
+					break;
+				case "java":
+					result = terminal("docker exec -i " + id + " /bin/bash -c 'cd ~/test/java && echo -e '" + input + "' >> test.java'");
+					result = terminal("docker exec -i " + id + " /bin/bash -c 'cd ~/test/java && javac -d Test.java'");
+					result = terminal("docker exec -i " + id + " /bin/bash -c 'cd ~/test/java && java Test.class'");
+					output = result.get(0);
+					break;
+				case "python":
+					result = terminal("docker exec -i " + id + " /bin/bash -c \"rm ~/test/python test.py\"");
+					result = terminal("docker exec -i " + id + " /bin/bash -c \"cd ~/test/python && echo -e '" + input + "' >> test.py\"");
+					result = terminal("docker exec -i " + id + " /bin/bash -c \"cd ~/test/python && python test.py\"");
+					output = result.get(0);
+					break;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return output;
+	}
+	
+	public Map<Integer, String> run(List<String> cmd) {
+		Map<Integer, String> res = new HashMap<>();
+		int code = 0;
+		
+		ProcessBuilder builder = new ProcessBuilder("C:\\Windows\\System32\\cmd.exe");
+		builder.command(cmd);
+		builder.redirectErrorStream(true);
+
+		try {
+			Process process = builder.start();
+			code = process.waitFor();
+			BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			String line;
+			StringBuilder sb = new StringBuilder();
+			while((line=stdout.readLine())!=null) {
+				sb.append(line + "\n");
+			}
+			System.out.println("stdout: " + sb.toString());
+			while((line=stderr.readLine())!=null) {
+				sb.append(line + "\n");
+			}
+			System.out.println("stderr: " + sb.toString());
+			String result = sb.toString();
+			res.put(code, result);
+			if(code != 0) {
+				throw new Exception(result);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+	
+	public Map<Integer, String> terminal(String cmd, String script) {
+		String[] split = cmd.split(" ");
+		List<String> cmds = new ArrayList<>();
+		for(String c : split) {
+			cmds.add(c);
+		}
+		cmds.add(script);
+		System.out.println(cmds);
+		return run(cmds);
+	}
+	
+	public Map<Integer, String> terminal(String cmd) {
+		String[] split = cmd.split(" ");
+		List<String> cmds = new ArrayList<>();
+		for(String c : split) {
+			cmds.add(c);
+		}
+		System.out.println(cmds);
+		return run(cmds);
+	}
+	
+	public Map<Integer, String> terminal(List<String> cmd) {
+		List<String> cmds = new ArrayList<>();
+		cmds.addAll(cmd);
+		System.out.println(cmds);
+		return run(cmds);
+	}
+	
+	public Map<Integer, String> exec(String id, List<String> cmd) {
+		List<String> cmds = new ArrayList<>();
+		cmds.add("docker");
+		cmds.add("exec");
+		cmds.add("-i");
+		cmds.add(id);
+		cmds.addAll(cmd);
+		return terminal(cmds);
+	}
+	
+	public Map<Integer, String> exec(String id, String[] cmd) {
+		List<String> cmds = new ArrayList<>();
+		cmds.add("docker");
+		cmds.add("exec");
+		cmds.add("-i");
+		cmds.add(id);
+		for(String c: cmd) {
+			cmds.add(c);
+		}
+		return terminal(cmds);
+	}
+}
