9ae73fd3542908fb74ef4166408b4939b7ae7de7
Azamman327 <wjsdbdud327@naver.com>
2023-06-18 02:08:36 +0900

    getSubjectList 오류수정

--- src/main/java/com/helloworld/controller/UserController.java
+++ src/main/java/com/helloworld/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000") 
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -32,27 +34,27 @@ public class UserController {
 
 	@GetMapping("/{userId}/subjects")
     public Map<Integer, Map<String, String>> getSubjectList(@PathVariable long userId) {
-		Map<String, String> classInfo = new HashMap<>();
+		Map<String, String> classInfo1 = new HashMap<>();
 		Map<Integer, Map<String, String>> classList = new HashMap<>();
 //		List<String> classList = new ArrayList<>();
 		
-		classInfo.put("classId", "1");
-		classInfo.put("className", "소프트웨어시스템개발");
-		classInfo.put("professor", "박창섭");
-		classInfo.put("period", "2023년 1학기");
-		classInfo.put("divide", "1");
+		classInfo1.put("classId", "1");
+		classInfo1.put("className", "소프트웨어시스템개발");
+		classInfo1.put("professor", "박창섭");
+		classInfo1.put("period", "2023년 1학기");
+		classInfo1.put("divide", "1");
 		
-		classList.put(1, classInfo);
+		classList.put(1, classInfo1);
 		
-		classInfo.clear();
+		Map<String, String> classInfo2 = new HashMap<>();
 		
-		classInfo.put("classId", "2");
-		classInfo.put("className", "문제해결기법");
-		classInfo.put("professor", "한혁");
-		classInfo.put("period", "2023년 1학기");
-		classInfo.put("divide", "3");
+		classInfo2.put("classId", "2");
+		classInfo2.put("className", "문제해결기법");
+		classInfo2.put("professor", "한혁");
+		classInfo2.put("period", "2023년 1학기");
+		classInfo2.put("divide", "3");
 		
-		classList.put(2, classInfo);
+		classList.put(2, classInfo2);
 		
 		return classList;
     }
