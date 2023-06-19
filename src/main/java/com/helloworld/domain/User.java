9f10c6227c99cf46f16730892339535af16e459e
Fornamin <97093507+Fornamin@users.noreply.github.com>
2023-06-19 21:28:40 +0900

    domain add : getter setter

--- src/main/java/com/helloworld/domain/User.java
+++ src/main/java/com/helloworld/domain/User.java
@@ -30,4 +30,54 @@ public class User implements Serializable {
 	@ManyToMany
 	@JoinTable()
 	private Set<SignUp> signUps;
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public List<Assignment> getAssignmentList() {
+		return assignmentList;
+	}
+
+	public void setAssignmentList(List<Assignment> assignmentList) {
+		this.assignmentList = assignmentList;
+	}
+	
+	
 }
