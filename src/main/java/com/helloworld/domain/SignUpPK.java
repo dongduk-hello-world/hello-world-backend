196a2a5bb23f2e91f011ef7e7741c54778145ed7
Fornamin <97093507+Fornamin@users.noreply.github.com>
2023-06-22 01:51:36 +0900

    signup pk

--- src/main/java/com/helloworld/domain/SignUpPK.java
+++ src/main/java/com/helloworld/domain/SignUpPK.java
@@ -1,5 +1,11 @@
 package com.helloworld.domain;
 
-public class SignUpPK {
+import java.io.Serializable;
 
+import lombok.Data;
+
+@Data
+public class SignUpPK implements Serializable {
+	private long user_id;
+	private long lecture_id;
 }
