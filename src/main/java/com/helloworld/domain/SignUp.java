63ec00985720db140424b8343e3e9d669365ee08
Fornamin <97093507+Fornamin@users.noreply.github.com>
2023-06-22 02:10:34 +0900

    error

--- src/main/java/com/helloworld/domain/SignUp.java
+++ src/main/java/com/helloworld/domain/SignUp.java
@@ -22,7 +22,7 @@ public class SignUp implements Serializable {
 	private long user_id;
 	@Id
 	@Column(name = "lecture_id")
-	private String lecture_id;
+	private long lecture_id;
 	
 	@ManyToMany(mappedBy="signUps")
 	private Set<User> users;
@@ -35,11 +35,11 @@ public class SignUp implements Serializable {
 		this.user_id = user_id;
 	}
 
-	public String getLecture_id() {
+	public long getLecture_id() {
 		return lecture_id;
 	}
 
-	public void setLecture_id(String lecture_id) {
+	public void setLecture_id(long lecture_id) {
 		this.lecture_id = lecture_id;
 	}
 	
